7d80fd1870b5474684ce39711d08534fbe92cf87
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2021-03-29 20:56:55 -0400

    Added menu sub-system to menu system

--- MenuSelect.h
+++ MenuSelect.h
@@ -6,6 +6,20 @@
 
 #define LEN(x) *(&x+1)-x  // get length of array
 
+uint8_t MenuDummy() { return true; }
+uint8_t MenuBack()  { return 2; }
+
+typedef uint8_t (*FuncMenuResult)();
+struct MenuInput {
+  String option;
+  FuncMenuResult funcMenuResult;
+
+  //MenuInput(String new_option, FuncMenuResult new_funcMenuResult) {
+    //option         = new_option;
+    //funcMenuResult = new_funcMenuResult;
+  //}
+};
+
 template <typename T, size_t size>
 struct MenuSelect {
   Menu    m;
@@ -16,12 +30,16 @@ struct MenuSelect {
     array = new_array;
   }
 
-  bool loop() {
-    if ( m.not_initialized() ) {
+  uint8_t loop() {
+    if ( m.is_active() ) {
+      if      ( (*array)[i+1].funcMenuResult() > 1 ) { return m.back(); }
+      else if ( (*array)[i+1].funcMenuResult() > 0 ) { m.back(); }
+    }
+    else if ( m.not_initialized() ) {
       HW::screen.clear();
-      if (!i) HW::screen.print(0,0,(*array)[i]);
-      else    HW::screen.print(0,0,"  " + (*array)[i]);
-      if ( i+1 < (uint8_t)size ) HW::screen.print(0,1," >" + (*array)[i+1]);
+      if (!i) HW::screen.print(0,0,(*array)[i].option);
+      else    HW::screen.print(0,0,"  " + (*array)[i].option);
+      if ( i+1 < (uint8_t)size ) HW::screen.print(0,1," >" + (*array)[i+1].option);
     }
     else if ( HW::knob.is_left() )  {
       if ( i > 0 ) {
@@ -35,14 +53,52 @@ struct MenuSelect {
       else               m.reinitialize(); 
     }
     else if ( HW::knob.is_pressed() ) {
-      uint8_t result = i+1; i = 0;
-      m.back(); return result;
+      m.activate();
     }
 
     return false;
   }
 
 };
+
+
+//template <typename T, size_t size>
+//struct MenuSelect {
+//  Menu    m;
+//  uint8_t i = 0;
+//  T       (*array)[size];
+//
+//  MenuSelect(T (*new_array)[size]) {
+//    array = new_array;
+//  }
+//
+//  bool loop() {
+//    if ( m.not_initialized() ) {
+//      HW::screen.clear();
+//      if (!i) HW::screen.print(0,0,(*array)[i]);
+//      else    HW::screen.print(0,0,"  " + (*array)[i]);
+//      if ( i+1 < (uint8_t)size ) HW::screen.print(0,1," >" + (*array)[i+1]);
+//    }
+//    else if ( HW::knob.is_left() )  {
+//      if ( i > 0 ) {
+//        i -= 1;
+//        m.reinitialize();
+//      }
+//    }
+//    else if ( HW::knob.is_right() ) {
+//      i += 1;
+//      if ( i >= size-1 ) i = size-2;
+//      else               m.reinitialize(); 
+//    }
+//    else if ( HW::knob.is_pressed() ) {
+//      uint8_t result = i+1; i = 0;
+//      m.back(); return result;
+//    }
+//
+//    return false;
+//  }
+//
+//};
 
 
 struct MenuListChoose {
--- Menus.h
+++ Menus.h
@@ -4,21 +4,38 @@
 #include "Menu.h"
 #include "MenuSelect.h"
 
-#define MS_NAME        1
-#define MS_BPM         2
-#define MS_SWITCHES    3
-#define MS_GBLSETTINGS 4
-#define MS_BACK        5
-String menu_settings_list[] = {"::SETTINGS::", "NAME", "BPM", "SWITCHES", "GBL SETTINGS", "BACK"};
-MenuSelect<String, LEN(menu_settings_list)> menu_settings(&menu_settings_list);
+MenuInput menu_input_gbl_settings[] = {
+  {"::GBL SETTINGS::", MenuDummy},
+  {"MIDI",             MenuDummy},
+  {"DEVICES",          MenuDummy},
+  {"BACK",             MenuBack}
+};
+MenuSelect<MenuInput, 4> menu_gbl_settings(&menu_input_gbl_settings);
 
-#define MG_DEVICES       1
-#define MG_MIDI_NOTES    2
-#define MG_COLORS        3
-#define MG_FACTORY_RESET 4
-#define MG_BACK          5
-String menu_global_list[] = {"::GBL SETTINGS::", "DEVICES", "MIDI NOTES", "COLORS", "FACTORY RESET", "BACK"};
-MenuSelect<String, LEN(menu_global_list)> menu_global(&menu_global_list);
+MenuInput menu_input_settings[] = {
+  {"::SETTINGS::", MenuDummy},
+  {"NAME",         MenuDummy},
+  {"BPM",          MenuDummy},
+  {"GBL SETTINGS", menu_gbl_settings},
+  {"BACK",         MenuBack}
+};
+MenuSelect<MenuInput, 5> menu_settings(&menu_input_settings);
+
+//#define MS_NAME        1
+//#define MS_BPM         2
+//#define MS_SWITCHES    3
+//#define MS_GBLSETTINGS 4
+//#define MS_BACK        5
+//String menu_settings_list[] = {"::SETTINGS::", "NAME", "BPM", "SWITCHES", "GBL SETTINGS", "BACK"};
+//MenuSelect<String, LEN(menu_settings_list)> menu_settings(&menu_settings_list);
+
+//#define MG_DEVICES       1
+//#define MG_MIDI_NOTES    2
+//#define MG_COLORS        3
+//#define MG_FACTORY_RESET 4
+//#define MG_BACK          5
+//String menu_global_list[] = {"::GBL SETTINGS::", "DEVICES", "MIDI NOTES", "COLORS", "FACTORY RESET", "BACK"};
+//MenuSelect<String, LEN(menu_global_list)> menu_global(&menu_global_list);
 
 
 #endif
